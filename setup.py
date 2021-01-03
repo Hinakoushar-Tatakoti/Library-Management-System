1fdcbb865158e2ed72237ad5fa49465083f8b3c5
Hinakoushar-Tatakoti <hinakoushar.tatakoti@gmail.com>
2021-01-03 23:37:19 +0530

    added build related  files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Library-Management-System',
+    version='1.0',
+    packages=['src', 'src.main', 'src.main.python', 'src.unittest', 'src.unittest.python'],
+    url='https://github.com/Hinakoushar-Tatakoti/Library-Management-System.git',
+    license='Beuth University Berlin',
+    author='Hinakoushar',
+    author_email='Hinakoushar.Tatakoti@outlook.com',
+    description='Library-Management-System'
+)
