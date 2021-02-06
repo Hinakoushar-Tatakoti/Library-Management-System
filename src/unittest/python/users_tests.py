17b1474eb42331d64beabc3945a39654e6440a40
Hinakoushar-Tatakoti <hinakoushar.tatakoti@gmail.com>
2021-02-06 20:07:49 +0530

    added changes for travis test

--- src/unittest/python/users_tests.py
+++ src/unittest/python/users_tests.py
@@ -6,15 +6,8 @@ class TestUser(TestCase):
     def setUp(self):
         self.user = User('Hina', 'admin')
 
-    def test_student_professors_actions(self):
-        try:
-            self.user.student_professors_actions()
-        except FileNotFoundError:
-            self.assertRaises(FileNotFoundError)
-
     def test_admin_actions(self):
         try:
             self.user.admin_actions()
         except FileNotFoundError:
             self.assertRaises(FileNotFoundError)
-
